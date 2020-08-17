2955e6ab71ce6eef27b9aef3017b72c1a2e0bd62
Gallagher <18225288277@163.com>
2020-08-17 18:57:49 +0800

    防守打法设定还是

--- JdbcDataSource/src/DataSource/Demo01C3P0.java
+++ JdbcDataSource/src/DataSource/Demo01C3P0.java
@@ -1,4 +1,18 @@
 package DataSource;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Demo01C3P0 {
+    public static void main(String[] args) throws SQLException {
+        //创建数据库连接池对象    这个里面可以指定参数配置 ComboPooledDataSource("otherc3p0");
+        DataSource ds = new ComboPooledDataSource();
+        //获取连接对象
+        Connection connection = ds.getConnection();
+        System.out.println(connection);  //这里我没有导入mysql相关的jar包：警告: Could not load driverClass com.mysql.jdbc.Driver
+    }
+
 }
---
+++ JdbcDataSource/src/DataSource/Demo02Druid.java
@@ -0,0 +1,43 @@
+package DataSource;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.util.Properties;
+
+/*
+1. 导入jar包： druid-1.0.9.jar
+2. 定义配置文件：
+        格式：properties
+        可以执行任意名称，放在任意路径下
+3.获取数据库连接池对象：通过工厂获取   DruidDataSourceFactory
+4.获取连接：getConnection
+
+ */
+public class Demo02Druid {
+    public static void main(String[] args) throws Exception {
+        //导入jar包
+        //定义配置文件,查找配置文件
+        Properties pro = new Properties();
+        ClassLoader classLoader = Demo02Druid.class.getClassLoader();
+        URL resource = classLoader.getResource("druid.properties");
+        String path = resource.getPath();
+        pro.load(new FileReader(path));
+        //获取数据库连接池对象
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(pro);
+        System.out.println(dataSource);
+        for (int i = 1; i <13 ; i++) {
+            Connection connection = dataSource.getConnection();
+            System.out.println(i + " " + connection);
+        }
+
+
+    }
+
+
+}
