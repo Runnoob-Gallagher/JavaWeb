65f1b5f114fb4f15b01a730c88c9028d585ac6ae
Gallagher <18225288277@163.com>
2020-08-21 18:40:10 +0800

    大叔大婶

---
+++ JavaScript/src/Syntax/Demo01Script.java
@@ -0,0 +1,13 @@
+package Syntax;
+/*增强用户和html页面的交互体验
+*   基本语法：
+*   1.与html结合方式
+*       *内部JS
+*       *外部JS
+*   2.注释
+*   3.数据类型
+*   4.变量
+*   5.运算符
+*   6.流程控制语句*/
+public class Demo01Script {
+}
