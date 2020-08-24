fdfa117babaf9a3e585d235b96148cd9ce8d034d
Gallagher <18225288277@163.com>
2020-08-24 19:49:13 +0800

    是多少

---
+++ JavaScript/src/BOM/Test04.java
@@ -0,0 +1,13 @@
+package BOM;
+
+public class Test04 {
+    static int i = 5;
+    public static void main(String[] args) {
+        i--;
+        if(i == 0){
+            System.out.println("结束");
+        }
+        System.out.println(i);
+
+    }
+}
