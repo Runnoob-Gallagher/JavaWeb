1b673d6281a76a964a5e9e8e150b0abe2fd00291
Gallagher <18225288277@163.com>
2020-09-09 00:57:43 +0800

    的撒

---
+++ CRUD/src/domain/Table_user.java
@@ -0,0 +1,80 @@
+package domain;
+
+public class Table_user {
+    private int id;
+    private String NAme;
+    private String gender;
+    private int age;
+    private String address;
+    private String qq;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNAme() {
+        return NAme;
+    }
+
+    public void setNAme(String NAme) {
+        this.NAme = NAme;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Table_user{" +
+                "id=" + id +
+                ", NAme='" + NAme + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", qq='" + qq + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
